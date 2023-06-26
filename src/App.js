bc1c9392682617661eddf49feffef9d084dc2442
Akash Gaurav <iamakashgaurav321@gmail.com>
2023-06-26 15:21:31 +0530

    Login Functionality Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import Layout from "./pages/layout";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+console.log(user)
   return (
-    <Layout />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <Layout values={{
+        user,
+        isAuthenticated,
+        isLoading
+      }} />
+    </Auth0Provider>
   );
 }
 
--- src/components/protected-component.js
+++ src/components/protected-component.js
@@ -1,8 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 
 const Protected = ({ component }) => {
     const Component = component
-    return <Component />
+    const { isAuthenticated } = useAuth0()
+    return isAuthenticated && <Component />
 }
 
 export default Protected
