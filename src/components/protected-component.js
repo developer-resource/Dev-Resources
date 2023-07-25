bbbfca0a9e6ade840aaec001f1053e30453cd2cc
Akash Gaurav <iamakashgaurav321@gmail.com>
2023-07-25 12:51:35 +0530

    Login Button Added and Landing Removed

--- src/components/protected-component.js
+++ src/components/protected-component.js
@@ -1,11 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 import LandingPage from '../pages/Landing/landing-page'
 
 const Protected = ({ component }) => {
     const Component = component
-    const { isAuthenticated } = useAuth0()
-    return isAuthenticated ? <Component /> : <LandingPage />
+    return <Component />
 }
 
 export default Protected
